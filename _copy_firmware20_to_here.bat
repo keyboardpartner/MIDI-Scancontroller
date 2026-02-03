f65cd89ed2f384bb593d9adcb70a9c4b2d3a5fea
HX3 Developer <info@keyboardpartner.de>
2026-02-03 14:39:40 +0100

    Changed to 20M Bootloader

--- _copy_firmware20_to_here.bat
+++ _copy_firmware20_to_here.bat
@@ -1,2 +1,2 @@
-type optiboot_atmega328.hex .\.pio\build\ATmega328P_20MHz\firmware.hex > firmware.hex
+type optiboot_atmega328_20M.hex .\.pio\build\ATmega328P_20MHz\firmware.hex > firmware.hex
 pause
