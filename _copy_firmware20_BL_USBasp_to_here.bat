c24dc1c3198716130617fe66f14fb9b1d57867b1
HX3 Developer <info@keyboardpartner.de>
2026-02-04 14:30:31 +0100

    New batch files

--- _copy_firmware20_BL_USBasp_to_here.bat
+++ _copy_firmware20_BL_USBasp_to_here.bat
@@ -1,2 +1,2 @@
-rem Copy Bootloader and rename to firmware.hex for arduino upload
+rem Merge Bootloader and firmware to one file, rename to firmware.hex for ISP programming
 type optiboot_atmega328_20M.hex .\.pio\build\ATmega328P_20MHz_USBasp\firmware.hex > firmware.hex
