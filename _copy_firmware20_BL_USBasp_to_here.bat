e9e58dce0021107dfd3d44ffbb5240303e1f5833
keyboardpartner <info@keyboardpartner.de>
2026-02-10 20:02:50 +0100

    not neede

--- _copy_firmware20_BL_USBasp_to_here.bat
+++
@@ -1,2 +0,0 @@
-rem Merge Bootloader and firmware to one file, rename to firmware.hex for ISP programming
-type optiboot_atmega328_20M.hex .\.pio\build\ATmega328P_20MHz_USBasp\firmware.hex > firmware.hex
